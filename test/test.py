ade40856301dc1deb7831685c1feda50dc28ab13
vg00164 <vrg2114@columbia.edu>
2019-04-23 13:04:56 -0400

    First Version of the TweetData Class and test class

---
+++ test/test.py
@@ -0,0 +1,32 @@
+import tweepy
+from datetime import datetime,timedelta
+import csv
+import json
+
+
+access_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_token_secret = "XXXXXXXXXXXXXXXXXXXXXXXXX"
+consumer_key = "49yh2KepopldjN9rf2a20isGj"
+consumer_secret = "Ryz1caenzOTPVod7RVYUOhFS8nMQHWAHw791EF3vtwyd17ChQY"
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+tweets = []
+search_terms = 'DIVYA OR IPL'
+fetched_tweets = tweepy.Cursor(api.search, q=search_terms, lang='en')
+listOfTweets=[]
+for tweet in fetched_tweets.items(3):
+    #listOfTweets.append(tweet._json)
+    dict_ = {'User Name': tweet.user.name,
+            'id_str' : tweet.id_str,
+            'Tweet Created At': tweet.created_at,
+            'Tweet Text': tweet.text,
+            'Retweeted': tweet.retweeted,
+            'Phone Type': tweet.source,
+            }
+    listOfTweets.append(dict_)
+    
+print(listOfTweets)
