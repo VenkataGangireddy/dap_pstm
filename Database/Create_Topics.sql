ba7363c2a24a057a4612e30a90845bea3ffa6d61
rohitdalal <dalal.rohit@yahoo.com>
2019-04-30 14:50:08 -0400

    Create Topics and Entities Record

---
+++ Database/Create_Topics.sql
@@ -0,0 +1,11 @@
+INSERT INTO dapproject.topics_tbl (Name, Description, Active_Flag) 
+	VALUES ('POLITICS', 'POLITICS', '1');
+
+INSERT INTO dapproject.topic_entities_tbl (Topic_Id, Entity_Type, Entity_Value, Active_Flag) 
+	VALUES ('1', 'KEYWORD', 'TRUMP', '1');
+INSERT INTO dapproject.topic_entities_tbl (Topic_Id, Entity_Type, Entity_Value, Active_Flag) 
+	VALUES ('1', 'KEYWORD', 'WHITE HOUSE', '1');
+INSERT INTO dapproject.topic_entities_tbl (Topic_Id, Entity_Type, Entity_Value, Active_Flag) 
+	VALUES ('1', 'KEYWORD', 'HEALTH CARE', '1');
+INSERT INTO dapproject.topic_entities_tbl (Topic_Id, Entity_Type, Entity_Value, Active_Flag) 
+	VALUES ('1', 'KEYWORD', 'IMMIGRATION', '1');
