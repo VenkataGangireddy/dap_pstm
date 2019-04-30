f0fe6cfe6ce7c895b15ffaee2b10e4b52075c244
rohitdalal <dalal.rohit@yahoo.com>
2019-04-30 14:44:32 -0400

    DDL changes to support MYSQL version 5.5

--- Database/DB_Objects.sql
+++ Database/DB_Objects.sql
@@ -1,74 +1,76 @@
 -- Create schema
-CREATE SCHEMA `ptsm` ;
+CREATE SCHEMA `dapproject` ;
 
 -- Create Tables
 
-CREATE TABLE `ptsm`.`topics_tbl` (
+CREATE TABLE `dapproject`.`topics_tbl` (
   `Topic_Id` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(45) NOT NULL,
-  `Description` VARCHAR(500) NULL,
+  `Name` VARCHAR(100) NOT NULL,
+  `Description` VARCHAR(200) NULL,
   `Active_Flag` INT NOT NULL,
-  `Created_Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Modified_Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Topic_Id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+  `Created_Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Modified_Date` TIMESTAMP,
+  PRIMARY KEY (`Topic_Id`),
+  UNIQUE KEY `topic_uk01` (`Name`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  
 
-CREATE TABLE `ptsm`.`topic_entities_tbl` (
+CREATE TABLE `dapproject`.`topic_entities_tbl` (
   `Topic_Entity_Id` INT NOT NULL AUTO_INCREMENT,
   `Topic_Id` INT NOT NULL,
   `Entity_Type` ENUM('HASHTAG', 'TWITTER_HANDLE', 'KEYWORD', 'OTHER') NOT NULL,
-  `Entity_Value` VARCHAR(500) NOT NULL,
+  `Entity_Value` VARCHAR(200) NOT NULL,
   `Active_Flag` INT NOT NULL,
-  `Created_Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Modified_Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Created_Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Modified_Date` TIMESTAMP,
   PRIMARY KEY (`Topic_Entity_Id`),
   UNIQUE KEY `topic_entities_uk01` (`Topic_Id`, `Entity_Type`, `Entity_Value`),
-  CONSTRAINT `topic_entities_fk01` FOREIGN KEY (`Topic_Id`) REFERENCES `ptsm`.`topics_tbl` (`Topic_Id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;  
+  CONSTRAINT `topic_entities_fk01` FOREIGN KEY (`Topic_Id`) REFERENCES `dapproject`.`topics_tbl` (`Topic_Id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;  
   
   
-CREATE TABLE `ptsm`.`twitter_data_tbl` (
+CREATE TABLE `dapproject`.`twitter_data_tbl` (
   `Tweet_Id` VARCHAR(100) NOT NULL,
-  `Tweet_Date` DATETIME,
-  `Text` VARCHAR(1000),
-  `Hashtag` VARCHAR(500),
-  `Expanded_Url` VARCHAR(1000),
-  `iso_language_code` VARCHAR(500),
-  `in_reply_to_status_id` VARCHAR(500),
-  `in_reply_to_user_id` VARCHAR(500),
-  `in_reply_to_screen_name` VARCHAR(500),
-  `User_Id` VARCHAR(500),  
-  `User_Name` VARCHAR(500),
-  `User_Screen_Name` VARCHAR(500),
-  `User_Location` VARCHAR(500),
-  `Original_Tweet_Id` INT,
-  `Original_Tweet_Date` DATETIME,
-  `Original_Text` VARCHAR(1000),
-  -- `Original_Hashtag` VARCHAR(500),
-  -- `Original_Expanded_Url` VARCHAR(5000),
-  -- `Original_iso_language_code` VARCHAR(500),
-  `Original_User_Id` VARCHAR(500),  
-  `Original_User_Name` VARCHAR(500),
-  `Original_User_Screen_Name` VARCHAR(500),
-  `Original_User_Location` VARCHAR(500),
+  `Tweet_Date` TIMESTAMP DEFAULT 0,
+  `Text` VARCHAR(250),
+  `Hashtag` VARCHAR(250),
+  `Expanded_Url` VARCHAR(250),
+  `iso_language_code` VARCHAR(250),
+  `in_reply_to_status_id` VARCHAR(250),
+  `in_reply_to_user_id` VARCHAR(250),
+  `in_reply_to_screen_name` VARCHAR(250),
+  `User_Id` VARCHAR(250),  
+  `User_Name` VARCHAR(250),
+  `User_Screen_Name` VARCHAR(250),
+  `User_Location` VARCHAR(250),
+  `Original_Tweet_Id` VARCHAR(100),
+  `Original_Tweet_Date` TIMESTAMP DEFAULT 0,
+  `Original_Text` VARCHAR(250),
+  -- `Original_Hashtag` VARCHAR(250),
+  -- `Original_Expanded_Url` VARCHAR(250),
+  -- `Original_iso_language_code` VARCHAR(250),
+  `Original_User_Id` VARCHAR(250),  
+  `Original_User_Name` VARCHAR(250),
+  `Original_User_Screen_Name` VARCHAR(250),
+  `Original_User_Location` VARCHAR(250),
   `Topic_Entity_Id` INT NOT NULL,
-  `Created_Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Modified_Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Created_Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Modified_Date` TIMESTAMP,
   PRIMARY KEY (`Tweet_Id`),
-  CONSTRAINT `twitter_data_fk01` FOREIGN KEY (`Topic_Entity_Id`) REFERENCES `ptsm`.`topic_entities_tbl` (`Topic_Entity_Id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;  
+  CONSTRAINT `twitter_data_fk01` FOREIGN KEY (`Topic_Entity_Id`) REFERENCES `dapproject`.`topic_entities_tbl` (`Topic_Entity_Id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;  
   
   
-CREATE TABLE `ptsm`.`twitter_sentiments_tbl` (
+CREATE TABLE `dapproject`.`twitter_sentiments_tbl` (
   `Tweet_Sentiment_Id` INT NOT NULL AUTO_INCREMENT,
   `Tweet_Id` VARCHAR(100) NOT NULL,
   `Topic_Entity_Id` INT NOT NULL,
   `Sentiment_Type` ENUM('POSITIVE', 'NEGATIVE', 'NEUTRAL') NOT NULL,
-  `Sentiment_Percentage` INT NOT NULL,
-  `Created_Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Modified_Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Sentiment_Percentage` INT,
+  `Created_Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Modified_Date` TIMESTAMP,
   PRIMARY KEY (`Tweet_Sentiment_Id`),
-  CONSTRAINT `twitter_sentiments_fk01` FOREIGN KEY (`Tweet_Id`) REFERENCES `ptsm`.`twitter_data_tbl` (`Tweet_Id`),
-  CONSTRAINT `twitter_sentiments_fk02` FOREIGN KEY (`Topic_Entity_Id`) REFERENCES `ptsm`.`topic_entities_tbl` (`Topic_Entity_Id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4; 
+  CONSTRAINT `twitter_sentiments_fk01` FOREIGN KEY (`Tweet_Id`) REFERENCES `dapproject`.`twitter_data_tbl` (`Tweet_Id`),
+  CONSTRAINT `twitter_sentiments_fk02` FOREIGN KEY (`Topic_Entity_Id`) REFERENCES `dapproject`.`topic_entities_tbl` (`Topic_Entity_Id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=UTF8; 
   
