f17dd8fe2afaf1e5991d757a48649078f5ac0bc9
rohitdalal <dalal.rohit@yahoo.com>
2019-05-10 15:47:37 -0400

    Added Stored Procedure to Create Event Logs

--- Database/Create Stored Procedures.sql
+++ Database/Create Stored Procedures.sql
@@ -10,3 +10,24 @@ SELECT a.topic_id,a.Name, b.Topic_Entity_Id,b.Entity_Type,b.Entity_Value
 END$$
 DELIMITER ;
 
+-- Procedure to Create Event Logs
+DELIMITER $$
+CREATE DEFINER=`dapproject`@`%` PROCEDURE `Create_Event_Log`(p_Topic_Entity_Id INT,
+                                                             p_Start_Time TIMESTAMP,
+                                                             p_End_Time  TIMESTAMP,
+                                                             p_Records_Processed INT,
+                                                             p_status  VARCHAR(10))
+BEGIN
+INSERT INTO events_log_tbl (Topic_Entity_Id,
+                            Start_Time,
+                            End_Time,
+                            Records_Processed,
+                            status)
+                   VALUES  (p_Topic_Entity_Id,
+                            p_Start_Time,
+                            p_End_Time,
+                            p_Records_Processed,
+                            p_status);
+COMMIT;                          
+END$$
+DELIMITER ;
